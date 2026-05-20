3f489a90b91a8bd523ae1f0f852ff9293a32d4e5
JP-code1427 <investateam1427@gmail.com>
2026-05-20 22:53:12 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use environment variable for API URL (falls back to localhost for development)
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://investa-jrwm.onrender.com/';
 
 // Create axios instance with default config
 const api = axios.create({
